3ab374c19e33c2687cebe9f9900412035e067364
Heath Brames <hbrames@umass.edu>
2020-09-26 14:17:44 -0400

    Navbar commit v2

--- src/components/navigation-bar/NavigationBar.jsx
+++ src/components/navigation-bar/NavigationBar.jsx
@@ -1,20 +1,55 @@
 import React from 'react';
+import './NavBar.css';
+import { Nav, Button, Navbar, Form, FormControl, DropdownButton, Dropdown, } from 'react-bootstrap';
 
-import { Nav, Button, Navbar, Form, FormControl,  } from 'react-bootstrap';
 
-export const NavigationBar = props => (
-  <Navbar bg="light" variant="light">
-    <Navbar.Brand href="">Hierarchy Heroes</Navbar.Brand>
-    <Nav className="mr-auto">
-      <Nav.Link href="/">Home</Nav.Link>
-    </Nav>
-    <Form inline>
-      <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-      <Button variant="outline-primary" className="mr-sm-2">Search</Button>
-    </Form>
-    <Form inline>
-      <Button variant="primary" href="/login">Log In</Button>
-    </Form>
-  </Navbar>
-  
-);
+const NavigationBar = (props) => {
+
+  if(props.isLoggedIn === 'false'){
+    return (
+      <Navbar className = "color-nav" variant="dark">
+        <Navbar.Brand className="title" href="/">Hierarchy Heroes</Navbar.Brand>
+        <Nav className="mr-auto">
+        </Nav>
+        <Form inline>
+          <Button variant="outline-light" href="/">Log In</Button>
+        </Form>
+      </Navbar>
+    );
+  }
+
+  else if(props.isLoggedIn === 'true'){
+    return (
+      <Navbar className = "color-nav" variant="dark">
+        <Navbar.Brand className="title" href="/">Hierarchy Heroes</Navbar.Brand>
+        <Nav className="mr-auto">
+        </Nav>
+        <Form inline>
+          <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+          <Button variant="outline-light" className="mr-sm-2">Search</Button>
+        </Form>
+        <DropdownButton className="account-btn" noCaret variant="outline-light" title=
+          {<div className="user-icon">
+          <i class="fas fa-user-circle" id="profile-icon"></i>
+          <i class="fas fa-caret-down" id="dropdown-icon"></i>
+          </div>} 
+          alignRight id="dropdown-menu-align-right">
+          <Dropdown.Item className="dropdown-items" eventKey="2">Name</Dropdown.Item>
+          <Dropdown.Item className="dropdown-items" eventKey="3">Job Title</Dropdown.Item>
+          <Dropdown.Item className="dropdown-items" eventKey="4">Project</Dropdown.Item>
+          <Dropdown.Item className="dropdown-items" eventKey="5">ID#</Dropdown.Item>
+          <Dropdown.Divider />
+          <Dropdown.Item href="/login" className="dropdown-items" eventKey="6">Logout<i class="fas fa-sign-out-alt"></i></Dropdown.Item>
+        </DropdownButton>
+      </Navbar>
+    );
+  }
+
+  else{
+    return null;
+  }
+
+};
+
+
+export default NavigationBar;
