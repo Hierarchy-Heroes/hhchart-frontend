074ec0cae7f6c975b1b5bba2b2e327603c726c1b
tcarns <68445375+tcarns@users.noreply.github.com>
2020-09-27 02:35:33 -0400

    login v1

--- src/components/login-page/LoginPage.jsx
+++ src/components/login-page/LoginPage.jsx
@@ -1,5 +1,31 @@
 import React from 'react';
+import './LoginPage.css';
+import { Button, Form, } from 'react-bootstrap';
 
-export const LoginPage = props => (
-  <span className="mx-auto">Log in page</span>
-);
+
+export const LoginPage = (props) => {
+  
+  return (
+    <Form>
+
+      <Form.Group className = "user mx-auto">
+        <Form.Label>Username</Form.Label>
+        <Form.Control type="user" placeholder="Enter username" />
+      </Form.Group>
+      <Form.Group className = "password mx-auto">
+        <Form.Label>Password</Form.Label>
+        <Form.Control type="password" placeholder="Enter password" />
+      </Form.Group>
+
+      <div class = "buttons">
+        <div class = "submit">
+          <Button type = "button" class = "btn btn-outline-primary">Login</Button>
+        </div>
+        <div class = "forgot">
+          <Button variant="outline-primary">Forgot password?</Button>
+        </div>
+      </div>
+
+    </Form>
+  );
+};
