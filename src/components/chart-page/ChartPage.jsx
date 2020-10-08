354bda5daa439ae351d608538933d29d46777797
Deniz <deniz.c.guler@gmail.com>
2020-10-08 17:57:04 -0400

    moved dummy data

--- src/components/chart-page/ChartPage.jsx
+++ src/components/chart-page/ChartPage.jsx
@@ -5,6 +5,21 @@ import Sidebar from './Sidebar';
 import { Button } from 'react-bootstrap';
 import './ChartPage.css';
 
+
+const list_data = [
+  { id: "n1", name: "Jamiya Alvarez", title: "Chief Executive Officer", children: ["n2", "n3", "n9", "n10"] },
+  { id: "n2", name: "Lewis Watts", title: "Chief Operations Officer" },
+  { id: "n3", name: "Bridget Fyre", title: "Chief Technology Officer", children: ["n4", "n5", "n8"] },
+  { id: "n4", name: "Tie Hua", title: "Senior Hardware Engineer" },
+  { id: "n5", name: "Kaeden Cameron", title: "Senior Software Engineer", children: ["n6", "n7"] },
+  { id: "n6", name: "Anabella Robbins", title: "Software Engineer I" },
+  { id: "n7", name: "Xiang Xiang", title: "Software Engineer I" },
+  { id: "n8", name: "Dereon Patel", title: "Software Engineer II" },
+  { id: "n9", name: "Shirley Knight", title: "Head Council" },
+  { id: "n10", name: "Carissa Rhodes", title: "Chief Marketing Officer", children: ["n11"] },
+  { id: "n11", name: "Raiden Mueller", title: "Marketing Director" }
+];
+
 const ds = {
   id: "n1",
   name: "Jamiya Alvarez",
@@ -56,7 +71,7 @@ export const ChartPage = props => {
       <div className={`bg-light border-right chartPageSidebar ${sidebarVisible ? "chartPageSidebarVisible" : ""}`}>
         <Sidebar node={currentNode} onClickClose={onClickClose}></Sidebar>
       </div>
-      <SearchBar visible={searchVisibile} handleClickOut={() => setSearchVisible(false)} />
+      <SearchBar data={list_data} visible={searchVisibile} handleClickOut={() => setSearchVisible(false)} />
       <OrgChart className="chartPageContentWrapper" datasource={ds} onClickNode={onClickNode} />
       <Button className="fab" onClick={() => setSearchVisible(!searchVisibile)}><i class="fas fa-search"></i></Button>
     </div>
--- src/components/search-bar/SearchBar.jsx
+++ src/components/search-bar/SearchBar.jsx
@@ -2,20 +2,6 @@ import React, { useState } from 'react';
 import './SearchBar.css';
 import Fuse from 'fuse.js';
 
-const data = [
-  { id: "n1", name: "Jamiya Alvarez", title: "Chief Executive Officer", children: ["n2", "n3", "n9", "n10"] },
-  { id: "n2", name: "Lewis Watts", title: "Chief Operations Officer" },
-  { id: "n3", name: "Bridget Fyre", title: "Chief Technology Officer", children: ["n4", "n5", "n8"] },
-  { id: "n4", name: "Tie Hua", title: "Senior Hardware Engineer" },
-  { id: "n5", name: "Kaeden Cameron", title: "Senior Software Engineer", children: ["n6", "n7"] },
-  { id: "n6", name: "Anabella Robbins", title: "Software Engineer I" },
-  { id: "n7", name: "Xiang Xiang", title: "Software Engineer I" },
-  { id: "n8", name: "Dereon Patel", title: "Software Engineer II" },
-  { id: "n9", name: "Shirley Knight", title: "Head Council" },
-  { id: "n10", name: "Carissa Rhodes", title: "Chief Marketing Officer", children: ["n11"] },
-  { id: "n11", name: "Raiden Mueller", title: "Marketing Director" }
-];
-
 const filter_results = (input, data) => {
   let search_prefix = 'name';
   if (input.includes(':')) {
@@ -59,7 +45,7 @@ const SearchBar = (props) => {
             value={inputValue}
             onChange={(e) => {
               setInputValue(e.target.value);
-              setResults(filter_results(e.target.value, data));
+              setResults(filter_results(e.target.value, props.data));
             }}
           />
           <label for='search'><i class="fas fa-search"></i></label>
