9df339cfe83d8399233de4be50191571e365efec
Deniz <deniz.c.guler@gmail.com>
2020-10-08 14:42:44 -0400

    started working on search function

--- src/components/search-bar/SearchBar.jsx
+++ src/components/search-bar/SearchBar.jsx
@@ -1,8 +1,28 @@
 import React, { useState } from 'react';
 import './SearchBar.css';
 
+const data = [
+  { id: "n1", name: "Jamiya Alvarez", title: "Chief Executive Officer", children: ["n2", "n3", "n9", "n10"]},
+  { id: "n2", name: "Lewis Watts", title: "Chief Operations Officer" },
+  { id: "n3", name: "Bridget Fyre", title: "Chief Technology Officer", children: ["n4", "n5", "n8"]},
+  { id: "n4", name: "Tie Hua", title: "Senior Hardware Engineer" },
+  { id: "n5", name: "Kaeden Cameron", title: "Senior Software Engineer", children: ["n6", "n7"]},
+  { id: "n6", name: "Anabella Robbins", title: "Software Engineer I" },
+  { id: "n7", name: "Xiang Xiang", title: "Software Engineer I" },
+  { id: "n8", name: "Dereon Patel", title: "Software Engineer II" },
+  { id: "n9", name: "Shirley Knight", title: "Head Council" },
+  { id: "n10", name: "Carissa Rhodes", title: "Chief Marketing Officer", children: ["n11"]},
+  { id: "n11", name: "Raiden Mueller", title: "Marketing Director" }
+];
+
 const filter_results = (input, data) => {
+  let search_prefix = 'name';
+  if (input.includes(':')) {
+    search_prefix = input.split(':')[0];
+  }
+  let search_dict = data.reduce((map, obj) => (map[obj[search_prefix]] = obj, map), {});
   
+  // console.log(search_prefix);
 }
 
 const Result = (props) => {
@@ -28,7 +48,7 @@ const SearchBar = (props) => {
             value={inputValue}
             onChange={(e) => {
               setInputValue(e.target.value);
-              setResults(filter_results(e.target.value, props.data));
+              setResults(filter_results(e.target.value, data));
             }}
           />
           <i class="fas fa-search"></i>
