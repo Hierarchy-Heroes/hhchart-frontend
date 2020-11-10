f12f66ecfec4bdab8f567ceea4291578c2d0226d
tcarns <68445375+tcarns@users.noreply.github.com>
2020-11-10 12:32:04 -0500

    Implemented UI for tree upload

--- src/components/navigation-bar/NavigationBar.jsx
+++ src/components/navigation-bar/NavigationBar.jsx
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './NavBar.css';
-import { Nav, Button, Navbar, Form, FormControl, DropdownButton, Dropdown, } from 'react-bootstrap';
+import { Nav, Button, Navbar, Form, FormControl, DropdownButton, Dropdown, Modal, } from 'react-bootstrap';
 import { useHistory } from 'react-router-dom';
 
 
 const NavigationBar = (props) => {
   const history = useHistory();
+
+  const [show, setShow] = useState(false)
+  const handleClose = () => setShow(false)
+  const handleShow = () => setShow(true)
 
   const logout = () => {
     window.sessionStorage.removeItem('authToken');
@@ -27,8 +31,8 @@ const NavigationBar = (props) => {
     return (
       <Navbar className = "color-nav" variant="dark">
         <Navbar.Brand className="title" href="/">Hierarchy Heroes</Navbar.Brand>
-        <Nav className="mr-auto">
-        </Nav>
+        <Nav className="mr-auto"></Nav>
+
         <DropdownButton className="account-btn" noCaret variant="outline-light" title=
           {<div className="user-icon">
           <i class="fas fa-user-circle" id="profile-icon"></i>
@@ -42,6 +46,26 @@ const NavigationBar = (props) => {
           <Dropdown.Divider />
           <Dropdown.Item href="/login" className="dropdown-items" eventKey="6" onClick={() => {logout()}}>Logout<i className="fas fa-sign-out-alt"></i></Dropdown.Item>
         </DropdownButton>
+        
+        <DropdownButton className="settings-btn" noCaret varient="outline-light" title = 
+          {<div className="user-icon">
+          <i class="fas fa-cog" id="profile-icon"></i>
+          <i class="fas fa-caret-down" id="dropdown-icon"></i>
+          </div>}
+          alignRight id="dropdown-menu-align-right">
+            <Dropdown.Item className="dropdown-items" eventKey="1" onClick={handleShow}>Import tree...</Dropdown.Item>
+          </DropdownButton>
+
+        <Modal show={show} onHide={handleClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>Import Tree</Modal.Title>
+          </Modal.Header>
+        <Modal.Body>Placeholder for file upload form</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>Close </Button>
+          <Button variant="primary" onClick={handleClose}>Upload</Button>
+        </Modal.Footer>
+        </Modal>
       </Navbar>
     );
   }
